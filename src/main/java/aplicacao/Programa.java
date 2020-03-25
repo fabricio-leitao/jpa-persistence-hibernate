6f8dda786f9447280f810d01955a38ac03e3d406
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-25 13:46:06 -0300

    Exemplo utilizando JPA Hibernate MySql

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,30 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		//Instanciando o Entity Manager Factory
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		//Instanciando o Entity Manager 
+		EntityManager em = emf.createEntityManager();
+		//Declarando a transação e abrindo ela
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		//Dando commit na transação
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
+	}
+}
