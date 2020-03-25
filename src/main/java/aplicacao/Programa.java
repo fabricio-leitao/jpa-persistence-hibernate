d9ee9513f002012492f95fa186679652ed90d63d
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-25 14:02:27 -0300

    Fazendo uma busca no banco de dados

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,21 +10,16 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
 		//Instanciando o Entity Manager Factory
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		//Instanciando o Entity Manager 
 		EntityManager em = emf.createEntityManager();
-		//Declarando a transação e abrindo ela
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		//Dando commit na transação
-		em.getTransaction().commit();
+		
+		//buscar um dado no banco de dados
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		
 		System.out.println("Pronto!");
 		//Fechando em e emf
 		em.close();
